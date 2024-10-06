965206974e6beee3a2d46cc26831014031b582b3
Cem Urgen <cem35520@gmail.com>
2024-10-06 00:43:49 -0400

    Add images, gameOver(), and gameReset() in script
    Added image compatibility to ui. Added a game over state once
    either player or bot reaches 5 wins. Added a gameReset state
    that resets the board for another game. Addtionaly, replaced image
    container div with img tag in index.html.

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const playerScoreValue = document.querySelector(".player-score-value");
 const botScoreValue = document.querySelector(".bot-score-value");
 
+const vs = document.querySelector(".vs");
 const result = document.querySelector(".result");
+
+const playerImage = document.querySelector(".player-image-container");
+const botImage = document.querySelector(".bot-image-container");
+
+const buttonContainer = document.querySelector(".buttons");
 
 const buttons = document.querySelectorAll("button");
 buttons.forEach(button => {
@@ -29,8 +35,6 @@ function getBotChoice() {
 function playRound(playerChoice, botChoice) {
     let roundResult = calcResult(playerChoice, botChoice);
 
-    //Update the result div text to reflect round result
-    result.textContent = roundResult;
 
     if (roundResult.includes("win")) {
         playerScore++;
@@ -39,6 +43,15 @@ function playRound(playerChoice, botChoice) {
     }
 
     updateScores();
+    updateImage(playerImage, playerChoice);
+    updateImage(botImage, botChoice);
+
+    if (playerScore === 5 || botScore === 5) {
+        gameOver();
+        return;
+    }
+
+    result.textContent = roundResult;
 }
 
 function calcResult(playerChoice, botChoice) {
@@ -56,4 +69,74 @@ function calcResult(playerChoice, botChoice) {
 function updateScores() {
     playerScoreValue.textContent = playerScore;
     botScoreValue.textContent = botScore;
+}
+
+function updateImage(image, choice) {
+    if (choice === "rock") {
+        if (image.className === "bot-image-container") {
+            image.setAttribute("src", "./images/mirrorRock.png");
+        } else {
+            image.setAttribute("src", "./images/rock.png");
+        }
+        image.setAttribute("alt", "A rock shaped hand.");
+    } 
+    if (choice === "paper") {
+        if (image.className === "bot-image-container") {
+            image.setAttribute("src", "./images/mirrorPaper.png");
+        } else {
+            image.setAttribute("src", "./images/paper.png");
+        }
+        image.setAttribute("alt", "A paper shaped hand.");
+    }
+    if (choice === "scissors") {
+        if (image.className === "bot-image-container") {
+            image.setAttribute("src", "./images/mirrorScissors.png");
+        } else {
+            image.setAttribute("src", "./images/scissors.png");
+        }
+        image.setAttribute("alt", "A scissor shaped hand");
+    }
+}
+
+function gameOver() {
+    vs.textContent = "Game Over";
+
+    if (playerScore === 5) {
+        result.textContent = "You won the game!";
+    } else if (botScore === 5) {
+        result.textContent = "The bot won the game";
+    }
+
+    buttons.forEach(button => {
+        button.style.display = "none";
+    });
+
+    const resetButton = document.createElement("button");
+    resetButton.textContent = "Reset";
+    resetButton.addEventListener("click", () => {
+        resetButton.remove();
+        resetGame();
+    });
+
+    buttonContainer.appendChild(resetButton);
+}
+
+function resetGame() {
+    playerScore = 0;
+    botScore = 0;
+
+    playerScoreValue.textContent = 0;
+    botScoreValue.textContent = 0;
+
+    playerImage.setAttribute("src", "data:image/gif;base64,R0lGODlhAQABAAAAACwAAAAAAQABAAA=");
+    botImage.setAttribute("src", "data:image/gif;base64,R0lGODlhAQABAAAAACwAAAAAAQABAAA=");
+    playerImage.setAttribute("alt", "");
+    botImage.setAttribute("alt", "");
+
+    vs.textContent = "VS";
+    result.textContent = "Choose from one of three buttons below!";
+
+    buttons.forEach(button => {
+        button.style.display = "block";
+    });
 }
